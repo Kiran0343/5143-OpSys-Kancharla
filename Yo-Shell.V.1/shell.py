5fdbc975f37c30e5b2de6aa4ff1c66dfa723db38
kiran reddy kancharla <kiran.kancharla92@gmail.com>
2016-03-01 23:22:10 +0530

    Update shell.py

--- Yo-Shell.V.1/shell.py
+++ Yo-Shell.V.1/shell.py
@@ -3,6 +3,9 @@
 1.Yashwanth Reddy  Muddireddy
 2.kiran Reddy Kancharla
 3.Srinivas Kanegave
+
+version : python 2
+
 "This code is to interpret shell through python"
 it takes the shell commands like copy,move,chmod,ls -l ,ls -a and more and implements as like of shell
 @Program Name: Yo-Shell V1 
